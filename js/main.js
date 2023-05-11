4ca9156a9fd7008c4a9d36e878b80f047a83cbe4
simi-22 <ssimii0222@gmail.com>
2023-05-11 15:43:12 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+const mMenuButton = document.querySelector('#mobile-menu');
+const mMFirst = document.querySelectorAll('#mobile-menu::after');
+const mMSecond = document.querySelector('#mobile-menu::before');
+const mMenu = document.querySelector('#mobile-roll');
+const playButton = document.querySelector('.ob-playbtn');
+const videoBox = document.querySelector('.object-video video');
+
+
+
+
+const mMenuActive = () => {
+    mMenu.classList.toggle('menu-height');
+}
+
+mMenuButton.addEventListener('click', mMenuActive)
+
+const objectVideoPlay = () => {
+    playButton.classList.add('hidden');
+    videoBox.classList.add('show');
+    setTimeout(stopVideo,33000);
+}
+
+const stopVideo = () => {
+    videoBox.classList.remove('show');
+    playButton.classList.remove('hidden');
+}
+
+
+
+playButton.addEventListener('click', objectVideoPlay)
